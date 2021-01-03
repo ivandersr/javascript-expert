b58d013768edbf2622806b73b7ad0f88dbcd9661
Ivander <ivandersalvadorruiz@gmail.com>
2021-01-02 21:06:23 -0300

    Testing security in regex and extracting data from text using FluentAPI

---
+++ modulo05/aula03-projeto01/app/test/util.test.js
@@ -0,0 +1,19 @@
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const { evaluateRegex, InvalidRegexError } = require('./../src/util');
+
+describe('Util', () => {
+  it('#evaluateRegex should throw an exception when an unsafe regex is '
+    + 'used', () => {
+      const unsafeRegex = /^([a-z|A-Z|0-9]+\s?)+$/;
+
+      expect(() => evaluateRegex(unsafeRegex)).to.throw(InvalidRegexError, `This ${unsafeRegex} is unsafe!`);
+    });
+
+  it('#evaluateRegex should not throw an exception when a safe regex is used',
+    () => {
+      const safeRegex = /^([a-z])$/;
+      expect(() => evaluateRegex(safeRegex)).to.not.throw;
+      expect(evaluateRegex(safeRegex)).to.be.ok;
+    });
+});
